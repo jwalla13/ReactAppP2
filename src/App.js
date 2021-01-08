e2424fc436dbe8f3d9567d979a6bcafa9029299b
Jonathan Wallach <jwalla13@calpoly.edu>
2021-01-08 14:59:40 -0800

    Add Delete Button

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import Table from './Table'
 
 class App extends Component {
-  render() {
-    const characters = [
+  state = {
+    characters: [
       {
         name: 'Charlie',
         job: 'Janitor',
@@ -21,12 +21,26 @@ class App extends Component {
         job: 'Bartender',
       },
     ]
+  }
 
+  removeCharacter = index => {
+    const { characters } = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  render() {
     return (
       <div className="container">
-        <Table characterData={characters}/>
+        <Table characterData={this.state.characters} removeCharacter={this.removeCharacter} />
       </div>
     )
   }
 }
+
+
 export default App
--- src/Table.js
+++ src/Table.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
 
-class Table extends Component {
-  render() {
-    const { characterData } = this.props
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={characterData} />
-      </table>
-    )
-  }
+const Table = props => {
+  const { characterData, removeCharacter } = props
+
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+    </table>
+  )
 }
+
 const TableHeader = () => {
   return (
     <thead>
@@ -28,6 +28,9 @@ const TableBody = props => {
       <tr key={index}>
         <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
       </tr>
     )
   })
