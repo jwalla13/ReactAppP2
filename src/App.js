a0dbf7edcdae3b375c11f6db4826bef8586f8062
Jonathan Wallach <jwalla13@calpoly.edu>
2021-01-08 15:12:26 -0800

    Create Form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form';
 
 class App extends Component {
   state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ]
+    characters: []
   }
 
   removeCharacter = index => {
@@ -37,6 +21,7 @@ class App extends Component {
     return (
       <div className="container">
         <Table characterData={this.state.characters} removeCharacter={this.removeCharacter} />
+        <Form />
       </div>
     )
   }
