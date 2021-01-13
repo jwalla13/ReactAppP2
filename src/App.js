53e20b0761d5a3785401359baadaa0770ce062fb
Jonathan Wallach <jwalla13@calpoly.edu>
2021-01-13 14:55:07 -0800

    Set up Post Calls

--- src/App.js
+++ src/App.js
@@ -8,6 +8,27 @@ class App extends Component {
     characters: []
   }
 
+  handleSubmit = character => {
+    this.makePostCall(character).then( callResult => {
+      if (callResult === true){
+        this.setState({ characters: [...this.state.characters, character] });
+      }
+    });
+  }
+
+  makePostCall(character){
+    return axios.post('http://localhost:5000/users', character)
+      .then(function (response) {
+        console.log(response);
+        return (response.status === 200);
+      })
+      .catch(function (error) {
+        console.log(error);
+        return false;
+      })
+
+  }
+
   componentDidMount() {
     axios.get('http://localhost:5000/users')
       .then(res => {
